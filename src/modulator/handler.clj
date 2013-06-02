38309a9ecf4e5c8756634d3dd2cf3489bd4859b6
Ticean Bennett <ticean@gmail.com>
2013-06-02 14:14:23 -0700

    Add image route

--- src/modulator/handler.clj
+++ src/modulator/handler.clj
@@ -5,6 +5,10 @@
 
 (defroutes app-routes
   (GET "/" [] "modulator")
+  (GET "/image/*" [* & params :as req]
+       (str "Will process image: " *
+            "<br/>With params: " params
+            "<br/><br/>Ring request: " req))
   (route/resources "/")
   (route/not-found "Not Found"))
 
--- test/modulator/test/handler.clj
+++ test/modulator/test/handler.clj
@@ -9,6 +9,10 @@
       (is (= (:status response) 200))
       (is (= (:body response) "modulator"))))
 
+  (testing "image route"
+    (let [response (app (request :get "/image/http://google.com/image.jpg"))]
+      (is (= (:status response) 200))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
