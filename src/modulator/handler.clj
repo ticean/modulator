9bc2ab07614dcd1783543aa98f78973e4ef6318f
Ticean Bennett <ticean@gmail.com>
2013-06-02 14:13:54 -0700

    Update response on default route

--- src/modulator/handler.clj
+++ src/modulator/handler.clj
@@ -4,7 +4,7 @@
             [compojure.route :as route]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "modulator")
   (route/resources "/")
   (route/not-found "Not Found"))
 
