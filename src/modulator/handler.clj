04f6af17ba0e89e90c366426da5ae27fdeb0ee94
Ticean Bennett <ticean@gmail.com>
2013-06-03 14:04:56 -0700

    Add parse-mods engine

--- src/modulator/handler.clj
+++ src/modulator/handler.clj
@@ -1,13 +1,15 @@
 (ns modulator.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [modulator.engine :as engine]))
 
 (defroutes app-routes
   (GET "/" [] "modulator")
   (GET "/image/*" [* & params :as req]
        (str "Will process image: " *
             "<br/>With params: " params
+            "<br/><br/>Will apply: <br/>" (engine/parse-mods engine/modulation-config params)
             "<br/><br/>Ring request: " req))
   (route/resources "/")
   (route/not-found "Not Found"))
